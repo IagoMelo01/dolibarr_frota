2ea79ff7743ff63c44b11c310b2014aab64dd8f1
BraianMelo <braian.ptu@gmail.com>
2025-10-14 19:16:06 -0300

    feat: historico.class.php implementada

---
+++ class/historico.class.php
@@ -0,0 +1,239 @@
+<?php
+/* Copyright (C) 2024 SuperAdmin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    frota/class/historico.class.php
+ * \ingroup frota
+ * \brief   Class file for Historico
+ */
+
+require_once DOL_DOCUMENT_ROOT.'/core/class/commonobject.class.php';
+
+/**
+ * Class for Historico
+ */
+class Historico extends CommonObject
+{
+    /**
+     * @var string ID of module.
+     */
+    public $module = 'frota';
+
+    /**
+     * @var string ID to identify managed object.
+     */
+    public $element = 'historico';
+
+    /**
+     * @var string Name of table without prefix where object is stored. This is also the key used for extrafields management.
+     */
+    public $table_element = 'frota_veiculo_historico';
+
+    /**
+     * @var int Does this object support multicompany module ?
+     * 0=No test on entity, 1=Test with field entity, 'field@table'=Test with link by field@table
+     */
+    public $ismultientitymanaged = 0;
+
+    /**
+     * @var int Does object support extrafields ? 0=No, 1=Yes
+     */
+    public $isextrafieldmanaged = 1;
+
+    /**
+     * @var string String with name of icon for historico. Must be the part after the 'object_' into object_historico.png
+     */
+    public $picto = 'fontawesome_fa-clock_fas';
+
+    const STATUS_DRAFT = 0;
+    const STATUS_VALIDATED = 1;
+    const STATUS_CANCELED = 9;
+
+    /**
+     *  'type' field format:
+     *  	'integer', 'integer:ObjectClass:PathToClass[:AddCreateButtonOrNot[:Filter[:Sortfield]]]',
+     *  	'select' (list of values are in 'options'),
+     *  	'sellist:TableName:LabelFieldName[:KeyFieldName[:KeyFieldParent[:Filter[:Sortfield]]]]',
+     *  	'chkbxlst:...',
+     *  	'varchar(x)',
+     *  	'text', 'text:none', 'html',
+     *  	'double(24,8)', 'real', 'price',
+     *  	'date', 'datetime', 'timestamp', 'duration',
+     *  	'boolean', 'checkbox', 'radio', 'array',
+     *  	'mail', 'phone', 'url', 'password', 'ip'
+     *		Note: Filter can be a string like "(t.ref:like:'SO-%') or (t.date_creation:<:'20160101') or (t.nature:is:NULL)"
+     */
+
+    /**
+     * @var array  Array with all fields and their property. Do not use it as a static var. It may be modified by constructor.
+     */
+    public $fields=array(
+        'rowid' => array('type'=>'integer', 'label'=>'TechnicalID', 'enabled'=>1, 'visible'=>-2, 'noteditable'=>1, 'notnull'=> 1, 'index'=>1, 'position'=>1, 'comment'=>'Id'),
+        'fk_veiculo' => array('type'=>'integer:Veiculo:frota/class/veiculo.class.php', 'label'=>'Veiculo', 'enabled'=>1, 'visible'=>1, 'position'=>30, 'notnull'=>1, 'index'=>1),
+        'data' => array('type'=>'date', 'label'=>'Data', 'enabled'=>1, 'visible'=>1, 'position'=>50, 'notnull'=>1),
+        'km' => array('type'=>'double(24,8)', 'label'=>'Quilometragem', 'enabled'=>1, 'visible'=>1, 'position'=>55, 'notnull'=>0),
+        'horimetro' => array('type'=>'double(24,8)', 'label'=>'Horímetro', 'enabled'=>1, 'visible'=>1, 'position'=>56, 'notnull'=>0),
+        'observacoes' => array('type'=>'text', 'label'=>'Observações', 'enabled'=>1, 'visible'=>1, 'position'=>60),
+        'date_creation' => array('type'=>'datetime', 'label'=>'DateCreation', 'enabled'=>1, 'visible'=>-2, 'notnull'=>1, 'position'=>500),
+        'tms' => array('type'=>'timestamp', 'label'=>'DateModification', 'enabled'=>1, 'visible'=>-2, 'notnull'=>1, 'position'=>501),
+        'fk_user_creat' => array('type'=>'integer:User:user/class/user.class.php', 'label'=>'UserAuthor', 'enabled'=>1, 'visible'=>-2, 'notnull'=>1, 'position'=>510, 'foreignkey'=>'user.rowid'),
+        'fk_user_modif' => array('type'=>'integer:User:user/class/user.class.php', 'label'=>'UserModif', 'enabled'=>1, 'visible'=>-2, 'position'=>511),
+        'import_key' => array('type'=>'varchar(14)', 'label'=>'ImportId', 'enabled'=>1, 'visible'=>-2, 'position'=>1000),
+        'status' => array('type'=>'integer', 'label'=>'Status', 'enabled'=>1, 'visible'=>1, 'position'=>1000, 'notnull'=>1, 'default'=>0, 'arrayofkeyval'=>array(0=>'Rascunho', 1=>'Validado', 9=>'Cancelado')),
+    );
+
+    public $rowid;
+    public $fk_veiculo;
+    public $data;
+    public $km;
+    public $horimetro; 
+    public $observacoes;
+    public $date_creation;
+    public $tms;
+    public $fk_user_creat;
+    public $fk_user_modif;
+    public $import_key;
+    public $status;
+
+    /**
+     * Constructor
+     *
+     * @param DoliDB $db Database handler
+     */
+    public function __construct(DoliDB $db)
+    {
+        global $conf, $langs;
+
+        $this->db = $db;
+
+        if (empty($conf->global->MAIN_SHOW_TECHNICAL_ID) && isset($this->fields['rowid'])) {
+            $this->fields['rowid']['visible'] = 0;
+        }
+        if (empty($conf->multicompany->enabled) && isset($this->fields['entity'])) {
+            $this->fields['entity']['enabled'] = 0;
+        }
+
+        // Unset fields that are disabled
+        foreach ($this->fields as $key => $val) {
+            if (isset($val['enabled']) && empty($val['enabled'])) {
+                unset($this->fields[$key]);
+            }
+        }
+
+        // Translate some data of arrayofkeyval
+        if (is_object($langs)) {
+            foreach ($this->fields as $key => $val) {
+                if (!empty($val['arrayofkeyval']) && is_array($val['arrayofkeyval'])) {
+                    foreach ($val['arrayofkeyval'] as $key2 => $val2) {
+                        $this->fields[$key]['arrayofkeyval'][$key2] = $langs->trans($val2);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Create object into database
+     *
+     * @param  User $user      User that creates
+     * @param  bool $notrigger false=launch triggers after, true=disable triggers
+     * @return int             <=0 if KO, Id of created object if OK
+     */
+    public function create(User $user, $notrigger = false)
+    {
+        return $this->createCommon($user, $notrigger);
+    }
+
+    /**
+     * Load object in memory from the database
+     *
+     * @param int    $id   Id object
+     * @param string $ref  Ref
+     * @return int         <0 if KO, 0 if not found, >0 if OK
+     */
+    public function fetch($id, $ref = null)
+    {
+        $result = $this->fetchCommon($id, $ref);
+        return $result;
+    }
+
+    /**
+     * Update object into database
+     *
+     * @param  User $user      User that modifies
+     * @param  bool $notrigger false=launch triggers after, true=disable triggers
+     * @return int             <0 if KO, >0 if OK
+     */
+    public function update(User $user, $notrigger = false)
+    {
+        return $this->updateCommon($user, $notrigger);
+    }
+
+    /**
+     * Delete object in database
+     *
+     * @param User $user       User that deletes
+     * @param bool $notrigger  false=launch triggers after, true=disable triggers
+     * @return int             <0 if KO, >0 if OK
+     */
+    public function delete(User $user, $notrigger = false)
+    {
+        return $this->deleteCommon($user, $notrigger);
+    }
+
+    /**
+     *  Return label of the status
+     *
+     *  @param  int     $mode          0=long label, 1=short label, 2=Picto + short label, 3=Picto, 4=Picto + long label, 5=Short label + Picto, 6=Long label + Picto
+     *  @return string                 Label of status
+     */
+    public function getLibStatut($mode = 0)
+    {
+        return $this->LibStatut($this->status, $mode);
+    }
+
+    /**
+     *  Return label of given status
+     *
+     *  @param  int     $status        Id status
+     *  @param  int     $mode          0=long label, 1=short label, 2=Picto + short label, 3=Picto, 4=Picto + long label, 5=Short label + Picto, 6=Long label + Picto
+     *  @return string                 Label of status
+     */
+    public function LibStatut($status, $mode = 0)
+    {
+        global $langs;
+
+        if (empty($this->labelStatus) || empty($this->labelStatusShort)) {
+            $this->labelStatus[self::STATUS_DRAFT] = $langs->transnoentitiesnoconv('Draft');
+            $this->labelStatus[self::STATUS_VALIDATED] = $langs->transnoentitiesnoconv('Validated');
+            $this->labelStatus[self::STATUS_CANCELED] = $langs->transnoentitiesnoconv('Canceled');
+            $this->labelStatusShort[self::STATUS_DRAFT] = $langs->transnoentitiesnoconv('Draft');
+            $this->labelStatusShort[self::STATUS_VALIDATED] = $langs->transnoentitiesnoconv('Validated');
+            $this->labelStatusShort[self::STATUS_CANCELED] = $langs->transnoentitiesnoconv('Canceled');
+        }
+
+        $statusType = 'status' . $status;
+        if ($status == self::STATUS_VALIDATED) {
+            $statusType = 'status4';
+        }
+        if ($status == self::STATUS_CANCELED) {
+            $statusType = 'status9';
+        }
+
+        return dolGetStatus($this->labelStatus[$status], $this->labelStatusShort[$status], '', $statusType, $mode);
+    }
+}
