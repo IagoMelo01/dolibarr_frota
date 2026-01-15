360705917ae4cbdb6dcc29200fece8276087cc0e
Braian <braian.ptu@gmail.com>
2026-01-15 15:44:55 -0300

    feat(Compra de Combustível): SQL modificado

--- sql/llx_frota_compracombustivel.sql
+++ sql/llx_frota_compracombustivel.sql
@@ -2,37 +2,57 @@
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
+-- the Free Software Foundation; either version 3 of the License, or
 -- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see https://www.gnu.org/licenses/.
 
-
-CREATE TABLE llx_frota_compracombustivel(
+CREATE TABLE llx_frota_compracombustivel (
 	-- BEGIN MODULEBUILDER FIELDS
-	rowid integer AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-	ref varchar(128) NOT NULL, 
-	label varchar(255), 
-	amount double DEFAULT NULL, 
-	qty real, 
-	fk_soc integer, 
-	fk_project integer, 
-	description text, 
-	note_public text, 
-	note_private text, 
-	date_creation datetime NOT NULL, 
-	tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
-	fk_user_creat integer NOT NULL, 
-	fk_user_modif integer, 
-	last_main_doc varchar(255), 
-	import_key varchar(14), 
-	model_pdf varchar(255), 
-	status integer NOT NULL
+	rowid integer AUTO_INCREMENT PRIMARY KEY NOT NULL,
+
+	ref varchar(128) NOT NULL,
+	label varchar(255),
+
+	-- Quantidade comprada (litros)
+	qty real NOT NULL,
+
+	-- Valor total da compra
+	amount double DEFAULT NULL,
+
+	-- Reservatório que recebe o combustível
+	fk_reservatorio integer NOT NULL,
+
+	-- Fornecedor
+	fk_soc integer,
+
+	-- Projeto
+	fk_project integer,
+
+	description text,
+	note_public text,
+	note_private text,
+
+	date_creation datetime NOT NULL,
+	tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+	fk_user_creat integer NOT NULL,
+	fk_user_modif integer,
+
+	last_main_doc varchar(255),
+	import_key varchar(14),
+	model_pdf varchar(255),
+
+	status integer NOT NULL,
 	-- END MODULEBUILDER FIELDS
+
+	-- Indexes
+	KEY idx_frota_compracombustivel_reservatorio (fk_reservatorio),
+	KEY idx_frota_compracombustivel_soc (fk_soc),
+	KEY idx_frota_compracombustivel_project (fk_project),
+
+	-- Foreign keys
+	CONSTRAINT fk_compracombustivel_reservatorio
+		FOREIGN KEY (fk_reservatorio)
+		REFERENCES llx_frota_reservatorio (rowid)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE
 ) ENGINE=innodb;
